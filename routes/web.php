5baf4de594ac4b3674d184cebb67132619ef34cd
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-02-04 16:50:36 +0700

    commit

---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\http\Controllers\ContactController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/home', function () {
+    return view('home', [
+        "title" => "Home"
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "title" => "About",
+        "nama" => "Sendiko",
+        "email" => "3103120150@student.smktelkom-pwt.sch.id",
+        "gambar" => "dsc.jpg"
+    ]);
+});
+
+Route::get('/gallery', function () {
+    return view('gallery', [
+        "title" => "Gallery"
+    ]);
+});
+
+Route::get('/contacts', function () {
+    return view('contacts', [
+        "title" => "Contacts"
+    ]);
+});
+
+Route::resource('/contacts', ContactController::class);
