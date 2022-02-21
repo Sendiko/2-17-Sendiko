5f85ab925bfd210258a4e4067a4dbcf537891bf4
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-02-21 07:06:54 +0700

    modul 6 selesai

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,38 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <!-- Required meta tags-->
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="au theme template">
-    <meta name="author" content="Hau Nguyen">
-    <meta name="keywords" content="au theme template">
+  <head>
+      <!-- Required meta tags-->
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta name="description" content="au theme template">
+      <meta name="author" content="Hau Nguyen">
+      <meta name="keywords" content="au theme template">
 
-    <!-- Title Page-->
-    <title>Dashboard</title>
-    <!-- CSS -->
-    @include('partials/css');
+      <!-- Title Page-->
+      <title>Dashboard</title>
+      <!-- CSS -->
+      @include('partials/css');
 
-</head>
+  </head>
 
-<body class="animsition">
-    <div class="page-wrapper">
+  <body class="animsition">
+      <div class="page-wrapper">
 
-        <!-- SIDEBAR -->
-        @include('partials/sidebar')
-        <!-- MOBILE HEADER -->
-        @include('partials/mobileheader')
-        <!-- PAGE CONTAINER-->
-        <div class="page-container">
-          <!-- HEADER -->
-            @include('partials/header')
-          <!-- MAIN CONTENT-->
-            @yield('content')
-        </div
+          <!-- SIDEBAR -->
+          @include('partials/sidebar')
+          <!-- MOBILE HEADER -->
+          @include('partials/mobileheader')
+          <!-- PAGE CONTAINER-->
+          <div class="page-container">
+            <!-- HEADER -->
+              @include('partials/header')
+            <!-- MAIN CONTENT-->
+              @yield('content')
+          </div
 
-    </div>
-    @include('partials/script');
-</body>
+      </div>
+      @include('partials/script');
+  </body>
 
 </html>
