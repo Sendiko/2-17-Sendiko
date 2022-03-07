054d750c1463758954b3d2887dd58869ab18dfaf
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-03-07 18:21:31 +0700

    code cleanup and change profile picture

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -17,13 +17,13 @@
                             </li>
                             <li>
                                 <a href="about.blade.php">Dash 2</a>
-                              </li>
-                              <li>
+                            </li>
+                            <li>
                                 <a href="gallery.blade.php">Dash 3</a>
-                              </li>
-                              <li>
+                            </li>
+                            <li>
                                 <a href="contact.blade.php">Dash 4</a>
-                              </li>
+                            </li>
                         </ul>
                 </li>
                 <li>
@@ -58,45 +58,6 @@
                         </li>
                         <li>
                             <a href="forget-pass.html">Forget Password</a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="has-sub">
-                    <a class="js-arrow" href="#">
-                        <i class="fas fa-desktop"></i>UI Elements</a>
-                    <ul class="list-unstyled navbar__sub-list js-sub-list">
-                        <li>
-                            <a href="button.html">Button</a>
-                        </li>
-                        <li>
-                            <a href="badge.html">Badges</a>
-                        </li>
-                        <li>
-                            <a href="tab.html">Tabs</a>
-                        </li>
-                        <li>
-                            <a href="card.html">Cards</a>
-                        </li>
-                        <li>
-                            <a href="alert.html">Alerts</a>
-                        </li>
-                        <li>
-                            <a href="progress-bar.html">Progress Bars</a>
-                        </li>
-                        <li>
-                            <a href="modal.html">Modals</a>
-                        </li>
-                        <li>
-                            <a href="switch.html">Switchs</a>
-                        </li>
-                        <li>
-                            <a href="grid.html">Grids</a>
-                        </li>
-                        <li>
-                            <a href="fontawesome.html">Fontawesome Icon</a>
-                        </li>
-                        <li>
-                            <a href="typo.html">Typography</a>
                         </li>
                     </ul>
                 </li>
