05b8650eb0dd6c372cc8efbb1c1c2194fbd95925
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-03-14 07:44:59 +0700

    code cleandup

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,34 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
 
-  <head>
-      <!-- META TAGS -->
-      @include('partials/meta')
+<head>
+    <!-- META TAGS -->
+    @include('partials/meta')
 
-      <!-- Title Page-->
-      <title>Dashboard</title>
-      <!-- CSS -->
-      @include('partials/css');
+    <!-- Title Page-->
+    <title>Dashboard</title>
+    <!-- CSS -->
+    @include('partials/css');
 
-  </head>
+</head>
 
-  <body class="animsition">
-      <div class="page-wrapper">
+<body class="animsition">
+    <div class="page-wrapper">
 
-          <!-- SIDEBAR -->
-          @include('partials/sidebar')
-          <!-- MOBILE HEADER -->
-          @include('partials/mobileheader')
-          <!-- PAGE CONTAINER-->
-          <div class="page-container">
-          <!-- HEADER -->
-          @include('partials/header')
-          <!-- MAIN CONTENT-->
-          @yield('content')
-          </div>
+        <!-- SIDEBAR -->
+        @include('partials/sidebar')
+        <!-- MOBILE HEADER -->
+        @include('partials/mobileheader')
+        <!-- PAGE CONTAINER-->
+        <div class="page-container">
+        <!-- HEADER -->
+        @include('partials/header')
+        <!-- MAIN CONTENT-->
+        @yield('content')
+        </div>
 
-      </div>
-      @include('partials/script');
-  </body>
+    </div>
+    @include('partials/script');
+</body>
 
 </html>
