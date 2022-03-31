95596b26b374bd3107ce4d8b847775ebaa58637b
Sendiko <3103120150@student.smktelkom-pwt.sch.id>
2022-03-31 16:06:17 +0700

    final commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.main')
-
+    <link rel="stylesheet" href="styles.css">
 @section('container')
     <h1>Halaman About</h1>
     <h3>{{$nama}}</h3>
     <p>{{$email}}</p>
-    <img src="images/{{$gambar}}" alt="{{$nama}}" width="200px"
+    <div id="image">
+        <img src="images/{{$gambar}}" alt="{{$nama}}" width="200px">
+    </div>
 @endsection
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,14 +1,14 @@
 <!-- NAVIGATION BAR -->
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
-        <a class="navbar-brand" href="#">Sendiko</a>
+        <a class="navbar-brand" href="/">Sendiko</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link {{ ($title === 'Beranda') ? 'active' : '' }}" aria-current="page" href="/index">Beranda</a>
+              <a class="nav-link {{ ($title === 'Beranda') ? 'active' : '' }}" aria-current="page" href="/">Beranda</a>
             </li>
             <li class="nav-item">
               <a class="nav-link {{ ($title === 'About') ? 'active' : '' }}" href="/about">About</a>
